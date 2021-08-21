9c965d0a96e8b37f98c270736e7c3665425dcc9f
Ebenezer Isaac <ebnezr.isaac@gmail.com>
2021-08-21 22:24:27 +0530

    java seminar question-12 (all questions completed, documentation remaining)

---
+++ sem-2/java/seminar/question-12/question-12-ejb/src/java/question12/MyListener.java
@@ -0,0 +1,33 @@
+package question11;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSDestinationDefinition;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+@JMSDestinationDefinition(name = "myTopic", interfaceName = "javax.jms.Topic", resourceAdapter = "jmsra", destinationName = "myTopic")
+@MessageDriven(activationConfig = {
+    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "myTopic")
+    ,
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic")
+})
+public class MyListener implements MessageListener {
+    
+    public MyListener() {
+    }
+    
+    @Override
+    public void onMessage(Message message) {
+        try {
+            TextMessage msg = (TextMessage) message;
+
+            System.out.println("Message Received :" + msg.getText());
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
---
+++ sem-2/java/seminar/question-12/question-12-war/src/java/MySender.java
@@ -0,0 +1,59 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.annotation.Resource;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Topic;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MySender extends HttpServlet {
+
+    @Resource(mappedName = "myTopic")
+    private Topic myTopic;
+
+    @Resource(mappedName = "myTopicConnectionFactory")
+    private ConnectionFactory Topic;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String message = request.getParameter("name");
+            sendJMSMessageToMyTopic(message);
+            out.print("Your name was sent successfully as message.");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    private void sendJMSMessageToMyTopic(String messageData) {
+        try {
+            Connection con = Topic.createConnection();
+            Session s = con.createSession();
+            MessageProducer mp = s.createProducer(myTopic);
+            TextMessage tm = s.createTextMessage();
+            tm.setText(messageData);
+            mp.send(tm);
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+}
